3ba98bf1c93562446fcb3005acf59b89ef6b38bb
premthak <premt9661@gmail.com>
2025-07-26 00:23:16 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -293,5 +293,6 @@ async def search_company(company_name: str):
 
 
 if __name__ == "__main__":
-  uvicorn app.main:app --host 0.0.0.0 --port $PORT
+  uvicorn backend.main:app --host 0.0.0.0 --port $PORT
+
 
